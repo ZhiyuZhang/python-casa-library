a6654c0e7a9c0ee079e96ae25983a24bbbdfbb9a
zhiyuzhang <pmozhang@gmail.com>
2019-12-20 22:23:39 +0800

    update make moments

--- makemoments.py
+++ makemoments.py
@@ -1,22 +1,19 @@
+## run in casa
+
 import os
 
 # -- import fits file without primary beam (PB) correction 
 #    This is because the noise+signal is uniform in this data. 
-importfits(imagename='CO_clean_robust2_nopb_niter500',fitsimage='CO_clean_robust2_nopb_niter500.fits',overwrite=True)
-
-# -- import fits file with PB correction -- for creating the final moment map. 
-importfits(imagename='CO_clean_robust2_nopb_niter500.pbcor',fitsimage='CO_clean_robust2_nopb_niter500.pbcor.fits',overwrite=True)
-
+importfits(imagename='13co_sub_sub',fitsimage='13co_sub.fits',overwrite=True)
 
 # -- subtract the same small regions for both data 
-imsubimage(imagename="CO_clean_robust2_nopb_niter500",           outfile="zoomin.image",       chans='20~70',region='box[[250pix,250pix], [1800pix, 1800pix]]',overwrite=True)
-imsubimage(imagename="CO_clean_robust2_nopb_niter500.pbcor", outfile="zoomin_pbcor.image", chans='20~70',region='box[[250pix,250pix], [1800pix, 1800pix]]',overwrite=True)
+imsubimage(imagename="13co_sub_sub",  outfile="zoomin.image",  region='',overwrite=True,chans='20~16364')
 
 # -- names of the images 
-img_pbcor = 'zoomin_pbcor.image'
 img       = 'zoomin.image'
 sm_img    = 'zoomin.image_sm'
 sm_sm_img = 'zoomin.image_sm_sm'
+final_img = 'final.image'
 
 
 # -- read header 
@@ -35,20 +32,29 @@ specsmooth(imagename=sm_img, outfile=sm_sm_img,  axis=2, dmethod="",width=2,func
 
 # -- define cutoff to be 3.5 sigma from the convolved datacube  
 #  This can be tuned, for optimising the final moment-0 map. 
-up_cutoff = 3.5  * imstat(sm_sm_img)['rms'][0]
+up_cutoff = 5  * imstat(sm_sm_img)['rms'][0]
 
-
-# --  make mask using up_cutoff on the smoothed, non-PB corrected datacube,  and apply the mask to the original, unmasked, PB corrected datacube.
+## --  make mask using up_cutoff on the smoothed, non-PB corrected datacube,  and apply the mask to the original, unmasked, PB corrected datacube.
 os.system("rm -rf mask*")
-ia.open(img_pbcor)
+ia.open(img)
 ia.calcmask(mask=str(sm_sm_img)+" > "+str(up_cutoff),name='masked_img')
 ia.close()
+#
 
 
-# -- Make moment0 image, using the masked, original resolution, PB-corrected datacube. 
-#  Selecting 16~40 channel number is the velocity range of about from 281~291 km/s  
-os.system("rm -rf zoomin_pbcor.image.mom0")
-immoments( imagename='zoomin_pbcor.image',chans='16~40',outfile='zoomin_pbcor.image.mom0') 
-exportfits(imagename='zoomin_pbcor.image.mom0',fitsimage='zoomin_pbcor.image.mom0.fits',overwrite=True)
+os.system("rm -rf     zoomin.image.mom0")
+immoments( imagename='zoomin.image',chans='10920~10950',outfile='zoomin.image.mom0') 
+exportfits(imagename='zoomin.image',fitsimage='output.fits',overwrite=True) 
 
+
+
+imsubimage(imagename="13co_sub_sub",  outfile="final.image",  region='',overwrite=True)
+os.system("rm -rf newmask.image")
+immath(imagename=['final.image','zoomin.image.mom0'],expr='IM0*(IM1*0+1)',outfile='newmask.image')
+
+
+makemask(mode='expand', inpimage='final.image', inpmask='zoomin.image:masked_img', inpfreqs='', outfreqs='', output='bigmask.im', overwrite=True)
+
+exportfits(imagename='newmask.image',fitsimage='newmask.image.fits',overwrite=True,velocity=True)
+#
   
