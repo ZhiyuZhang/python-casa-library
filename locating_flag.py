303ba152e8487b10dbf5e28f9184055cb469a65b
zhiyuzhang <pmozhang@gmail.com>
2020-02-06 13:06:00 +0800

    Update locating_flag.py

--- locating_flag.py
+++ locating_flag.py
@@ -1,10 +1,21 @@
-# funtion to identify flagging data from the output in the logfile
+# Aim: A function for identifying tagged data (by plotms) to flag, read from the output in the logfile.
+# Usage: under CASA;  version (>5.6)
+# Within CASA:  plotms, select a region, click 'locate'. 
+# Then the selected base information will shown in the log file and the message window. 
+# Then the script will read the most frequent baselines and their statistics, 
+# and output flagdata command for your choice.
+
+# Example:
+# execfile('locating_flag.py')
 
 # Author: Zhi-Yu Zhang
 # Email: pmozhang@gmail.com 
 # History:
 #   21 Mar 2019, update by Zhiyu
 #   30 Sep 2019, transfer into function from the original script, by Jianhang
+#   06 Feb 2020, update searching using Regular Expression  
+
+
 
 import os
 import glob
@@ -12,59 +23,72 @@ from   collections import Counter
 
 def locating_flag(logfile):
     string       = open(logfile,'rU')
-    all          = str(string.readlines())
-    LastEndNum   = all.rfind('End')
-    LastSection  = str(all[LastEndNum:])
+    AllLines     = str(string.readlines())
+    LastEndNum   = AllLines.rfind('Plotting')
+    LastEndNum   = max(AllLines.rfind(i) for i in ["Plotting", "END", "UVdist", "Time in"])# AllLines.rfind('Plotting')
 
-    ant1 = ''
-    ant2 = ''
-    corr = ''
 
-    ants =[]
+    LastSection  = str(AllLines[LastEndNum:])
+    ant1         = ''
+    ant2         = ''
+    corr         = ''
+    ants         = []
 
     for line in LastSection.split('\\n'):
         if line.find('BL=')!=-1:
-           a    = line.find('BL=')
-           b    = line.find('Corr=')
-           c    = line.find('Time=')
-           d    = line.find('Spw=')
-           e    = line.find('Scan=')
-           ant1 = str(line[a+3 :a+7 ])
-           ant2 = str(line[a+15:a+19])
+           ant1  = re.compile("BL"    + b'(.*?)' + "&"    , re.S).findall(line)[0][1:].rstrip()
+           ant2  = re.compile("&"     + b'(.*?)' + "\["   , re.S).findall(line)[0][1:].rstrip()
+           scan  = re.compile("Scan"  + b'(.*?)' + "Field", re.S).findall(line)[0][1:].rstrip()
+           field = re.compile("Field" + b'(.*?)' + "\["   , re.S).findall(line)[0][1:].rstrip()
+           time  = re.compile("Time"  + b'(.*?)' + "BL"   , re.S).findall(line)[0][1:].rstrip()
+           spw   = re.compile("Spw"   + b'(.*?)' + "Chan" , re.S).findall(line)[0][1:].rstrip()
+           chan  = re.compile("Chan"  + b'(.*?)' + "Freq" , re.S).findall(line)[0][1:].rstrip()
+           corr  = re.compile("Corr"  + b'(.*?)' + "X="   , re.S).findall(line)[0][1:].rstrip()
            ants.append(ant1)
            ants.append(ant2)
-           corr = str(line[b+5 :b+7 ])
-           time = str(line[c+16:c+29])
-           spw  = str(line[d+4 :d+6 ])
-           scan = str(line[e+5 :e+7 ])
-    #      print("----")
+           print("flagdata(vis,mode='manual',antenna='"+ant1+"&"+ant2+"',correlation='"+corr+"', spw='"+spw+"',scan='"+scan+"',timerange='"+time+"')")
+        elif line.find('ANT1=')!=-1:
+           ant1  = re.compile("ANT1"  + b'(.*?)' + "&"    , re.S).findall(line)[0][1:].rstrip()
+           scan  = re.compile("Scan"  + b'(.*?)' + "Field", re.S).findall(line)[0][1:].rstrip()
+           field = re.compile("Field" + b'(.*?)' + "\["   , re.S).findall(line)[0][1:].rstrip()
+           time  = re.compile("Time"  + b'(.*?)' + "ANT1" , re.S).findall(line)[0][1:].rstrip()
+           spw   = re.compile("Spw"   + b'(.*?)' + "Chan" , re.S).findall(line)[0][1:].rstrip()
+           chan  = re.compile("Chan"  + b'(.*?)' + "Freq" , re.S).findall(line)[0][1:].rstrip()
+           corr  = re.compile("Corr"  +b'(.*?)'  + "X="   , re.S).findall(line)[0][1:].rstrip()
+           ants.append(ant1)
            print("flagdata(vis,mode='manual',antenna='"+ant1+"&"+ant2+"',correlation='"+corr+"', spw='"+spw+"',scan='"+scan+"',timerange='"+time+"')")
 
-    #print(sorted(ants, key=Counter(ants).get, reverse=True))
 
-    topten = Counter(ants).most_common(3)
-    freq = Counter(ants)
+    topten = Counter(ants).most_common(10)
+    freq   = Counter(ants)
 
+
+#   print("-----------------")
+#   print(topten)
     print("-----------------")
-    print(topten)
-    print("-----------------")
-    print("Most frequent antennas and their frequency: ")
+    print("Most frequent baselines and their frequencies: ")
     print("                        ")
+    
 
-    try:
-        most_ant1 = topten[0][0]
-        print(topten[0][0], freq[topten[0][0]])
-        most_ant2 = topten[1][0]
-        print(topten[1][0], freq[topten[1][0]])
-        most_ant3 = topten[2][0]
-        print(topten[2][0], freq[topten[2][0]])
-    except:
-        pass
+    most_baselines= [] 
+    for idx in range(0,10):
+        if len(topten) > idx:
+            most_baselines.append(topten[idx][0])
+            print(topten[idx][0], freq[topten[idx][0]])
+
+
     print("                        ")
     print("-----------------")
-    print("flagdata(vis, mode='manual',antenna='"+str(most_ant1)+"')")
+    for idx in range(0, len(most_baselines)):
+        print("flagdata(vis, mode='manual',antenna='"+str(most_baselines[idx])+"')")
     print("-----------------")
 
+
+    string.close()
+
+if __name__ == '__main__':
+    # directly run in ipython shell
+    locating_flag(casalog.logfile())
 
 
 
@@ -77,8 +101,4 @@ def locating_flag(logfile):
     # # --------------------------------------------------
 
 
-    string.close()
 
-if __name__ == '__main__':
-    # directly run in ipython shell
-    locating_flag(casalog.logfile())
